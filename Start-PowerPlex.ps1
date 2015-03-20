b19090aac0f331446b2cd10a89cf532ebb8846a2
coolmacool <rshacklef0rd@outlook.com>
2015-03-19 19:02:51 -0500

    PowerPlex HelperFunctions cleanup
    Cleanup of HelperFunctions module

--- Start-PowerPlex.ps1
+++ Start-PowerPlex.ps1
@@ -1,10 +1,13 @@
 ﻿Clear-Host
 
+[Console]::TreatControlCAsInput = $true
+
 $Script:PowerPlex = @{ ScriptDirectory = (Split-Path $MyInvocation.MyCommand.Path -Parent) }
 $Script:PowerPlex.AssetsDirectory = $Script:PowerPlex.ScriptDirectory + "\Assets"
 
 Get-ChildItem (Join-Path $Script:PowerPlex.ScriptDirectory *.ps1) | ? { $_.FullName -ne $MyInvocation.MyCommand.Path} | % { . $_.FullName;Write-Output "Loading $($_.FullName)"; }
 
+Test-ElevatedPowerShell
 
 Update-Console "***"
 Update-Console "PowerPlex"
